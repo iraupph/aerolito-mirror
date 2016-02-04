cfe53c5d3d31a6ac849be5564e5ce401c97a8ea5
Ícaro Raupp <budicaro@gmail.com>
2016-02-04 19:05:26 -0200

    turning on and off on headset input

---
+++ MagicMirror/app/src/main/java/aerolito/magicmirror/ui/activity/MainActivity.java
@@ -0,0 +1,131 @@
+package aerolito.magicmirror.ui.activity;
+
+import android.content.ContentResolver;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Handler;
+import android.provider.Settings;
+import android.support.v7.app.AppCompatActivity;
+import android.view.KeyEvent;
+import android.view.View;
+import android.widget.Toast;
+
+import aerolito.magicmirror.BuildConfig;
+import aerolito.magicmirror.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final int HIDE_UI_DELAY = 1000;
+
+    private static final int SLEEP_DELAY = 15 * 1000;
+    private static final int WAKE_UP_DELAY = 0;
+
+    private static final int OFF_BRIGHTNESS = 0;
+    private static final int ON_BRIGHTNESS = 89; // Brilho é regulado de 0 até 255 (89 é 35%)
+
+    private Handler uiChangesHandler;
+
+    private Handler wakeUpHandler;
+    private Handler sleepHandler;
+    private View overlay;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // View que fica por cima do nosso conteúdo
+        overlay = findViewById(R.id.overlay);
+
+        // Listener pra caso alguém toque na tela esconder as barras do sistema que vão aparecer
+        getWindow().getDecorView().setOnSystemUiVisibilityChangeListener
+                (new View.OnSystemUiVisibilityChangeListener() {
+                    @Override
+                    public void onSystemUiVisibilityChange(int visibility) {
+                        if ((visibility & View.SYSTEM_UI_FLAG_FULLSCREEN) == 0) {
+                            // Ambas barras estão visíveis, vamos esconder de novo com um delay
+                            makeFullscreen();
+                        }
+                    }
+                });
+
+        uiChangesHandler = new Handler();
+        wakeUpHandler = new Handler();
+        sleepHandler = new Handler();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        makeFullscreen();
+
+        wakeUpNow();
+        refreshScheduledSleep();
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if (keyCode == KeyEvent.KEYCODE_HEADSETHOOK) {
+            // Ligamos a tela (brilho no máximo) e agendamos pra apagar (brilho no mínimo) com um delay
+            if (BuildConfig.D) {
+                Toast.makeText(getApplicationContext(), "RECEIVED JACK INPUT!", Toast.LENGTH_SHORT).show();
+            }
+            wakeUpNow();
+            refreshScheduledSleep();
+        }
+        return true;
+    }
+
+    /**
+     * Desliga a tela (diminui o brilho) e esconde o conteúdo após um delay de {@link MainActivity#SLEEP_DELAY}
+     */
+    private void refreshScheduledSleep() {
+        // Remover qualquer agendamento...
+        sleepHandler.removeCallbacksAndMessages(null);
+        // ... e criar um novo
+        sleepHandler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                if (BuildConfig.D) {
+                    Toast.makeText(getApplicationContext(), "SCREEN OFF", Toast.LENGTH_SHORT).show();
+                }
+                ContentResolver cResolver = getApplicationContext().getContentResolver();
+                Settings.System.putInt(cResolver, Settings.System.SCREEN_BRIGHTNESS, OFF_BRIGHTNESS);
+                overlay.setVisibility(View.VISIBLE);
+            }
+        }, SLEEP_DELAY);
+    }
+
+    /**
+     * Liga a tela (aumenta o brilho) e mostra o conteúdo após um delay de {@link MainActivity#WAKE_UP_DELAY}
+     */
+    private void wakeUpNow() {
+        wakeUpHandler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                ContentResolver cResolver = getApplicationContext().getContentResolver();
+                Settings.System.putInt(cResolver, Settings.System.SCREEN_BRIGHTNESS, ON_BRIGHTNESS);
+                overlay.setVisibility(View.GONE);
+                if (BuildConfig.D) {
+                    Toast.makeText(getApplicationContext(), "SCREEN ON", Toast.LENGTH_SHORT).show();
+                }
+            }
+        }, WAKE_UP_DELAY);
+    }
+
+    /**
+     * Força fullscreen, escondendo a barra de status (topo) e navegação (em baixo) após um pequeno delay
+     */
+    private void makeFullscreen() {
+        uiChangesHandler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                int systemUiFlagHideNavigation = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION;
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
+                    systemUiFlagHideNavigation |= View.SYSTEM_UI_FLAG_FULLSCREEN;
+                }
+                getWindow().getDecorView().setSystemUiVisibility(systemUiFlagHideNavigation);
+            }
+        }, HIDE_UI_DELAY);
+    }
+}
